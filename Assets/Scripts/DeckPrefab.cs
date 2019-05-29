5d5f89af75fa5e4e695ce2378e37dd856492a77c
AmeliaShields <the.amelia.s@gmail.com>
2019-05-29 10:35:45 -0700

    Prefab generator

--- Assets/Scripts/DeckPrefab.cs
+++ Assets/Scripts/DeckPrefab.cs
@@ -8,17 +8,18 @@ public class DeckPrefab : MonoBehaviour
 {
 
     [FormerlySerializedAs("CardPrefab")] public GameObject cardPrefab;
-    public Sprite kingOfHearts;
-    //download it here https://opengameart.org/sites/default/files/styles/medium/public/king_of_hearts2.png
-    // Start is called before the first frame update
+    Sprite[] cardSprites = Resources.LoadAll<Sprite>("Sprites/fullCardDeck");
     void Start()
     {
-        for (int i = 0; i < 1; i++)
+        for (int i = 0; i < 52; i++)
         {         
             GameObject cardInstance = Instantiate(cardPrefab, this.transform);
-            //Debug.Log(cardInstance.GetComponent<Card>().suit);
+            cardInstance.name = ("Card" + i);
+            Debug.Log(i);
+         
+
             
-            cardInstance.GetComponent<SpriteRenderer>().sprite = kingOfHearts;
+            cardInstance.GetComponent<SpriteRenderer>().sprite = cardSprites[i];
             cardInstance.transform.position = new Vector3(2 * i - 8, 0f, 0f);
             Debug.Log("Created Instance " + i);
             cardInstance.transform.localScale -= new Vector3(.5F, .5F, 0);
