ac9eb40675add538e517fda714fedc4b3a1ae6e3
AmeliaShields <the.amelia.s@gmail.com>
2019-05-31 10:43:37 -0700

    Edit

--- Assets/Scripts/Draggables.cs
+++ Assets/Scripts/Draggables.cs
@@ -5,9 +5,20 @@ using UnityEngine.EventSystems;
 
 public class Draggables : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-
+     Vector3 startPosition;
+     Transform startParent;
+     public GameObject canvas;
+     private Transform canvasTransform;
+     public static GameObject itemBeingDragged;
+    
     public void OnBeginDrag(PointerEventData eventData){
         Debug.Log("OnBeginDrag");
+        itemBeingDragged = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        canvasTransform = canvas.transform;
+        transform.parent = canvasTransform;
 }
 
     public void OnDrag(PointerEventData eventData){
